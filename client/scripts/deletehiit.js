e7aedc90e7260547e328a6483fd6bd04ef8fb0e1
Paschal Ezenwobi <ezenwobikene@gmail.com>
2024-05-21 15:16:35 +0100

    delete pop-up implemented

--- client/scripts/deletehiit.js
+++ client/scripts/deletehiit.js
@@ -1,33 +1,3 @@
-// const ui = {};
-
-// export async function deleteHiit(hiit) {
-//   const toBeDeleted = document.querySelector(
-//     `.${hiit.name.replace(/\s+/g, '')}`
-//   );
-//   const response = await fetch(`/hiits/${hiit.hiits_id}`, {
-//     method: 'DELETE',
-//   });
-//   if (response.ok) {
-//     toBeDeleted.remove();
-//   } else {
-//     console.log('Failed to delete HIIT', response);
-//     toBeDeleted.style.display = 'grid';
-//   }
-// }
-
-// export function removeHiitBeforeDelete(hiit, event) {
-//   const toBeDeleted = document.querySelector(`.${hiit.name.replace(/\s+/g, '')}`);
-//   event.stopPropagation();
-//   if (!toBeDeleted) {
-//     return;
-//   }
-//   toBeDeleted.style.display = 'none';
-//   ui.eventInfo = document.querySelector('.event-info');
-// }
-
-
-
-
 const ui = {};
 
 export async function deleteHiit(hiit) {
